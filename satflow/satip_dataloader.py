34e76373186e30e24f35735490fb7333abd92407
Josh Millar <s1827995@uhtred.inf.ed.ac.uk>
2022-02-22 15:25:30 +0000

    nksdna

--- satflow/satip_dataloader.py
+++ satflow/satip_dataloader.py
@@ -1,9 +1,11 @@
 import dask
+from numbers import Number
 import pytorch_lightning as pl
 import torch
 from torch.utils.data import random_split, DataLoader, Dataset
 import xarray
 import numpy as np
+import pandas as pd
 
 class CustomDataset(Dataset):
     def __init__(self, images, future_images):
@@ -23,7 +25,7 @@ class DataModuleClass(pl.LightningDataModule):
     def __init__(self):
         super().__init__()
         self.download_dir = ''
-        self.batch_size = 32
+        self.batch_size = 16
         self.pin_memory: bool = True
         self.configuration_filename="satflow/configs/masks.yaml"
 
@@ -45,15 +47,47 @@ class DataModuleClass(pl.LightningDataModule):
         data_array = dataset["data"]
         self.data_array = data_array.sortby('time')
 
+    def get_spatial_region_of_interest(self, data_array, x_index_at_center: Number,
+                                       y_index_at_center: Number
+                                       ) -> xarray.DataArray:
+        x_and_y_index_at_center = pd.Series(
+            {"x": x_index_at_center, "y": y_index_at_center}
+        )
+        half_image_size_pixels = 256 // 2
+        min_x_and_y_index = x_and_y_index_at_center - half_image_size_pixels
+        max_x_and_y_index = x_and_y_index_at_center + half_image_size_pixels
+        suggested_reduction_of_image_size_pixels = (
+                max(
+                    (-min_x_and_y_index.min() if (min_x_and_y_index < 0).any() else 0),
+                    (max_x_and_y_index.x - len(data_array.x)),
+                    (max_x_and_y_index.y - len(data_array.y)),
+                )
+                * 2
+        )
+        if suggested_reduction_of_image_size_pixels > 0:
+            new_suggested_image_size_pixels = (
+                256 - suggested_reduction_of_image_size_pixels
+        )
+        data_array = data_array.isel(
+            x=slice(min_x_and_y_index.x, max_x_and_y_index.x),
+            y=slice(min_x_and_y_index.y, max_x_and_y_index.y),
+        )
+        return data_array
+
     def setup(self, stage=None):
-        self.data_array = self.data_array[:50]
+        regions = []
+        centers = [(512,512)]
+        for (x_osgb,y_osgb) in centers:
+            regions.append(self.get_spatial_region_of_interest(self.data_array,x_osgb,y_osgb))
         print("Processing...0%")
-        X_tensors = [self.transform(timestep[:-1]) for timestep in self.data_array]
+        X_tensors = [self.transform(timestep[:-1]) for timestep in regions]
+        X_tensors = self.listify(X_tensors)
         print("Processing...25%")
-        y_tensors = [self.transform(timestep[1:]) for timestep in self.data_array]
+        y_tensors = [self.transform(timestep[1:]) for timestep in regions]
+        y_tensors = self.listify(y_tensors)
         print("Processing...50%")
-        X_tensors = [torch.reshape(t,[1,890, 1843]) for t in X_tensors]
-        y_tensors = [torch.reshape(t,[1,890, 1843]) for t in y_tensors]
+        X_tensors = [torch.reshape(t,[1,256, 256]) for t in X_tensors]
+        y_tensors = [torch.reshape(t,[1,256, 256]) for t in y_tensors]
         X_t = list(zip(*[iter(X_tensors)]*5))
         X_t = [torch.stack(x) for x in X_t][:-1]
         #X = [torch.reshape(x, [1,5,1,890, 1843]) for x in X_t][:-1]
@@ -61,6 +95,8 @@ class DataModuleClass(pl.LightningDataModule):
         y_t = list(zip(*[iter(y_tensors)]*5))
         y_t = [torch.stack(y) for y in y_t][:-1]
         print("Processing...100%")
+        print(X_t[0].shape)
+        print(y_t[0].shape)
         dataset = CustomDataset(X_t,y_t)
         #dataset = list(zip(X,y))
         train_size = int(0.8 * len(y_t))
@@ -68,8 +104,8 @@ class DataModuleClass(pl.LightningDataModule):
         self.train_data, self.val_data = torch.utils.data.random_split(dataset, [train_size, val_size])
 
     def train_dataloader(self):
-        return DataLoader(self.train_data, batch_size=self.batch_size, pin_memory=True, drop_last=False)
+        return DataLoader(self.train_data, batch_size=self.batch_size, pin_memory=True, drop_last=False, num_workers=8)
 
     def val_dataloader(self):
-        return DataLoader(self.val_data, batch_size=self.batch_size, pin_memory=True, drop_last=False)
+        return DataLoader(self.val_data, batch_size=self.batch_size, pin_memory=True, drop_last=False, num_workers=8)
 
